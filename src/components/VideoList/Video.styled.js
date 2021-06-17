5ddefa78727d3ab17345a8bdd60c87e75118d29b
Quang Trinh <quang@maffiaco.com>
2021-06-17 18:06:12 +0700

    Fixed max lines for title and description

--- src/components/VideoList/Video.styled.js
+++ src/components/VideoList/Video.styled.js
@@ -24,11 +24,23 @@ export const StyledVideoCover = styled.div`
 `;
 
 export const StyledVideoTitle = styled.div`
-  font-size: 24px;
+  font-size: 1.5rem;
+  line-height: 1.5rem;
   font-weight: semi-bold;
   margin: 0.75rem;
+  height: calc(1.5rem * 2);
+
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `;
 
 export const StyledVideoDescription = styled.div`
   margin: 0.75rem;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `;
