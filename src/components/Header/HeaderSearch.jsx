aa87fb38ed9b9e11596210deecbd2a04087f8d10
Quang Trinh <quang@maffiaco.com>
2021-07-13 17:03:12 +0700

    Fix search query synced

--- src/components/Header/HeaderSearch.jsx
+++ src/components/Header/HeaderSearch.jsx
@@ -7,8 +7,7 @@ import SearchInput from '../SearchInput';
 
 export default function HeaderSearch() {
   const { query, dispatch } = useContext(YoutubeQueryContext);
-
-  const [q, setQ] = useState('');
+  const [q, setQ] = useState(query);
 
   const onSearchInputChange = (e) => {
     setQ(e.target.value);
@@ -35,7 +34,7 @@ export default function HeaderSearch() {
         aria-label="Search through site content"
         placeholder="Search..."
         onChange={onSearchInputChange}
-        value={query}
+        value={q}
       />
     </form>
   );
